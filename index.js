ffc5d7dafeead0d5205ea4a1ed428b3861585531
Jonathan Pool <pool@stulta.com>
2022-03-06 19:55:05 -0500

    Debugged

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ const addItems = async (query, itemType, isSelect) => {
   const itemNames = await fs.readdir(`.data/${key}`);
   const itemJSONs = [];
   for (const itemName of itemNames) {
-    itemJSONs.push(await fs.readFile(`.data/${key}/${itemName}.json`));
+    itemJSONs.push(await fs.readFile(`.data/${key}/${itemName}`));
   }
   query[size] = itemJSONs.length;
   query[key] = itemJSONs.map(itemJSON => {
