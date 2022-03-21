c6f5eafc827777a9eec450ccb4ee303886947c1f
Jonathan Pool <jonathan.pool@cvshealth.com>
2022-03-21 17:05:58 -0400

    Continued developing

--- digesters/asp09.js
+++ digesters/asp09.js
@@ -30,6 +30,8 @@ exports.parameters = (report, query) => {
     `<p>Summary of the details:</p>${joiner}<ul>${innerJoiner}${failures}${joiner}</ul>`;
   const customResult = (score, test, failures) =>
     `${customFailText(score, test)}${joiner}${customFailMore(failures)}`;
+  // Returns the act of a test.
+  const actOf = testName => report.acts.filter(act => act.type === 'test' && act.which === testName);
   // Get general data.
   query.dateISO = report.endTime.slice(0, 10);
   query.dateSlash = query.dateISO.replace(/-/g, '/');
@@ -37,49 +39,70 @@ exports.parameters = (report, query) => {
   query.scoreProc = __filename.slice(0, -3);
   query.org = report.host.what;
   query.url = report.host.which;
-  const {deficit} = scoreData;
-  query.totalScore = deficit.total;
-  // Get summary-table data for scoreDoc.
-  const deficitData = Object.assign({}, deficit, scoreData.inferences);
-  const deficitTypes = Object.keys(deficitData);
-  query.deficitRows = deficitTypes
-  .sort((a, b) => deficitData[b] - deficitData[a])
-  .map(type => `<tr><th>${type}</th><td>${deficitData[type]}</td></tr>`)
+  const scoreAct = report.acts.filter(act => act.type === 'score')[0];
+  const {result} = scoreAct;
+  const {inferences, scores} = result;
+  query.totalScore = scores.total;
+  // Get summary-table data.
+  const netScores = Object.assign({}, scores, inferences);
+  const scoreSources = Object.keys(netScores);
+  query.scoreRows = scoreSources
+  .sort((a, b) => netScores[b] - netScores[a])
+  .map(source => `<tr><th>${source}</th><td>${netScores[source]}</td></tr>`)
   .join(innerJoiner);
-  query.scoreTable = JSON.stringify(scoreData.deficit, null, 2).replace(/^\{\n|[}","]/g, '');
-  // Get package-test result messages for scoreDoc.
+  // Get package-test result messages.
   // aatt
-  if (deficit.aatt) {
-    const aattWarnings = new Set(testData.aatt.result.filter(item => item.type === 'warning')
+  if (scores.aatt) {
+    const testAct = actOf('aatt');
+    const aattWarnings = new Set(testAct.result.filter(item => item.type === 'warning')
     .map(item => `warning: ${item.msg}`));
-    const aattErrors = new Set(testData.aatt.result.filter(item => item.type === 'error')
+    const aattErrors = new Set(testAct.result.filter(item => item.type === 'error')
     .map(item => `error: ${item.msg}`));
     const aattBoth = Array.from(aattWarnings).concat(Array.from(aattErrors));
-    const aattFailures = aattBoth.map(item => `<li>${htmlEscape(item)}</li>`).join(innerJoiner);
-    query.aattResult = packageFailText(deficit.aatt, 'aatt', aattFailures);
+    const aattIssues = aattBoth.map(item => `<li>${htmlEscape(item)}</li>`).join(innerJoiner);
+    query.aattResult = packageFailText(scores.aatt, 'aatt', aattIssues);
   }
-  else if (scoreData.inferences.aatt) {
-    query.aattResult = testCrashText(deficitData.aatt, 'aatt');
+  else if (inferences.aatt) {
+    query.aattResult = testCrashText(inferences.aatt, 'aatt');
   }
   else {
     query.aattResult = packageSucceedText('aatt');
   }
+  // alfa
+  if (scores.alfa) {
+    const testAct = actOf('alfa');
+    const alfaWarnings = new Set(testAct.result.filter(item => item.verdict === 'cantTell')
+    .map(item => `warning: ${item.rule.ruleSummary}`));
+    const alfaErrors = new Set(testAct.result.filter(item => item.verdict === 'failed')
+    .map(item => `error: ${item.rule.ruleSummary}`));
+    const alfaBoth = Array.from(alfaWarnings).concat(Array.from(alfaErrors));
+    const alfaIssues = alfaBoth.map(item => `<li>${htmlEscape(item)}</li>`).join(innerJoiner);
+    query.alfaResult = packageFailText(scores.alfa, 'alfa', alfaIssues);
+  }
+  else if (inferences.alfa) {
+    query.alfaResult = testCrashText(inferences.alfa, 'alfa');
+  }
+  else {
+    query.alfaResult = packageSucceedText('alfa');
+  }
   // axe
-  if (deficit.axe) {
-    const axeFailures = testData.axe.result.items.map(
+  if (scores.axe) {
+    const testAct = actOf('axe');
+    const axeFailures = testAct.result.items.map(
       item => `<li>${item.rule}: ${htmlEscape(item.description)}</li>`
     ).join(innerJoiner);
-    query.axeResult = packageFailText(deficit.axe, 'axe', axeFailures);
+    query.axeResult = packageFailText(scores.axe, 'axe', axeFailures);
   }
-  else if (scoreData.inferences.axe) {
-    query.axeResult = testCrashText(deficitData.axe, 'axe');
+  else if (inferences.axe) {
+    query.axeResult = testCrashText(inferences.axe, 'axe');
   }
   else {
     query.axeResult = packageSucceedText('axe');
   }
   // ibm
-  if (deficit.ibm) {
-    const {result} = testData.ibm;
+  if (scores.ibm) {
+    const testAct = actOf('ibm');
+    const {result} = testAct;
     const contentItems = result.content.items;
     const urlItems = result.url.items;
     const items = [];
@@ -92,17 +115,18 @@ exports.parameters = (report, query) => {
     const ibmFailures = Array.from(new Set(items.map(
       item => `<li>${item.ruleId}: ${htmlEscape(item.message)}</li>`
     )).values()).join(innerJoiner);
-    query.ibmResult = packageFailText(deficit.ibm, 'ibm', ibmFailures);
+    query.ibmResult = packageFailText(scores.ibm, 'ibm', ibmFailures);
   }
-  else if (scoreData.inferences.ibm) {
-    query.ibmResult = testCrashText(deficitData.ibm, 'ibm');
+  else if (inferences.ibm) {
+    query.ibmResult = testCrashText(inferences.ibm, 'ibm');
   }
   else {
     query.ibmResult = packageSucceedText('ibm');
   }
   // wave
-  if (deficit.wave) {
-    const waveResult = testData.wave.result.categories;
+  if (scores.wave) {
+    const testAct = actOf('wave');
+    const waveResult = testAct.result.categories;
     const waveItems = [];
     ['error', 'contrast', 'alert'].forEach(category => {
       waveItems.push(
@@ -112,172 +136,185 @@ exports.parameters = (report, query) => {
       );
     });
     const waveFailures = waveItems.join(innerJoiner);
-    query.waveResult = packageFailText(deficit.wave, 'wave', waveFailures);
+    query.waveResult = packageFailText(scores.wave, 'wave', waveFailures);
   }
-  else if (scoreData.inferences.wave) {
-    query.waveResult = testCrashText(deficitData.wave, 'wave');
+  else if (inferences.wave) {
+    query.waveResult = testCrashText(inferences.wave, 'wave');
   }
   else {
     query.waveResult = packageSucceedText('wave');
   }
-  // Get custom-test result messages for scoreDoc.
-  if (deficit.bulk) {
-    query.bulkResult = `The page <strong>did not pass</strong> the <code>bulk</code> test. The count of visible elements in the page was ${testData.bulk.result.visibleElements}, resulting in a score of ${deficit.bulk} on <code>bulk</code>.`;
+  // Get custom-test result messages.
+  if (scores.bulk) {
+    const testAct = actOf('bulk');
+    query.bulkResult = `The page <strong>did not pass</strong> the <code>bulk</code> test. The count of visible elements in the page was ${testAct.result.visibleElements}, resulting in a score of ${scores.bulk} on <code>bulk</code>.`;
   }
-  else if (scoreData.inferences.bulk) {
-    query.bulkResult = testCrashText(deficitData.bulk, 'bulk');
+  else if (inferences.bulk) {
+    query.bulkResult = testCrashText(inferences.bulk, 'bulk');
   }
   else {
     query.bulkResult = customSucceedText('bulk');
   }
-  if (deficit.embAc) {
-    const failures = customFailures(testData.embAc.result.totals);
-    query.embAcResult = customResult(deficit.embAc, 'embAc', failures);
+  if (scores.embAc) {
+    const testAct = actOf('embAc');
+    const failures = customFailures(testAct.result.totals);
+    query.embAcResult = customResult(scores.embAc, 'embAc', failures);
   }
-  else if (scoreData.inferences.embAc) {
-    query.embAcResult = testCrashText(deficitData.ebmAc, 'ebmAc');
+  else if (inferences.embAc) {
+    query.embAcResult = testCrashText(inferences.ebmAc, 'ebmAc');
   }
   else {
     query.embAcResult = customSucceedText('embAc');
   }
-  if (deficit.focAll) {
-    const failures = customFailures(testData.focAll.result);
-    query.focAllResult = customResult(deficit.focAll, 'focAll', failures);
+  if (scores.focAll) {
+    const testAct = actOf('focAll');
+    const failures = customFailures(testAct.result);
+    query.focAllResult = customResult(scores.focAll, 'focAll', failures);
   }
-  else if (scoreData.inferences.focAll) {
-    query.focAllResult = testCrashText(deficitData.focAll, 'focAll');
+  else if (inferences.focAll) {
+    query.focAllResult = testCrashText(inferences.focAll, 'focAll');
   }
   else {
     query.focAllResult = customSucceedText('focAll');
   }
-  if (deficit.focInd) {
-    const failSource = testData.focInd.result.totals.types;
+  if (scores.focInd) {
+    const testAct = actOf('focInd');
+    const failSource = testAct.result.totals.types;
     const failObj = {
       indicatorMissing: failSource.indicatorMissing.total,
       nonOutlinePresent: failSource.nonOutlinePresent.total
     };
     const failures = customFailures(failObj);
-    query.focIndResult = customResult(deficit.focInd, 'focInd', failures);
+    query.focIndResult = customResult(scores.focInd, 'focInd', failures);
   }
-  else if (scoreData.inferences.focInd) {
-    query.focIndResult = testCrashText(deficitData.focInd, 'focInd');
+  else if (inferences.focInd) {
+    query.focIndResult = testCrashText(inferences.focInd, 'focInd');
   }
   else {
     query.focIndResult = customSucceedText('focInd');
   }
-  if (deficit.focOp) {
-    const failSource = testData.focOp.result.totals.types;
+  if (scores.focOp) {
+    const testAct = actOf('focOp');
+    const failSource = testAct.result.totals.types;
     const failObj = {
       onlyFocusable: failSource.onlyFocusable.total,
       onlyOperable: failSource.onlyOperable.total
     };
     const failures = customFailures(failObj);
-    query.focOpResult = customResult(deficit.focOp, 'focOp', failures);
+    query.focOpResult = customResult(scores.focOp, 'focOp', failures);
   }
-  else if (scoreData.inferences.focOp) {
-    query.focOpResult = testCrashText(deficitData.focOp, 'focOp');
+  else if (inferences.focOp) {
+    query.focOpResult = testCrashText(inferences.focOp, 'focOp');
   }
   else {
     query.focOpResult = customSucceedText('focOp');
   }
-  if (deficit.hover) {
-    const failures = customFailures(testData.hover.result.totals);
-    query.hoverResult = customResult(deficit.hover, 'hover', failures);
+  if (scores.hover) {
+    const testAct = actOf('hover');
+    const failures = customFailures(testAct.result.totals);
+    query.hoverResult = customResult(scores.hover, 'hover', failures);
   }
-  else if (scoreData.inferences.hover) {
-    query.hoverResult = testCrashText(deficitData.hover, 'hover');
+  else if (inferences.hover) {
+    query.hoverResult = testCrashText(inferences.hover, 'hover');
   }
   else {
     query.hoverResult = customSucceedText('hover');
   }
-  if (deficit.labClash) {
-    const {totals} = testData.labClash.result;
+  if (scores.labClash) {
+    const testAct = actOf('labClash');
+    const {totals} = testAct.result;
     delete totals.wellLabeled;
     const failures = customFailures(totals);
-    query.labClashResult = customResult(deficit.labClash, 'labClash', failures);
+    query.labClashResult = customResult(scores.labClash, 'labClash', failures);
   }
-  else if (scoreData.inferences.labClash) {
-    query.labClashResult = testCrashText(deficitData.labClash, 'labClash');
+  else if (inferences.labClash) {
+    query.labClashResult = testCrashText(inferences.labClash, 'labClash');
   }
   else {
     query.labClashResult = customSucceedText('labClash');
   }
-  if (deficit.linkUl) {
-    const failures = customFailures(testData.linkUl.result.totals.inline);
-    query.linkUlResult = customResult(deficit.linkUl, 'linkUl', failures);
+  if (scores.linkUl) {
+    const testAct = actOf('linkUl');
+    const failures = customFailures(testAct.result.totals.inline);
+    query.linkUlResult = customResult(scores.linkUl, 'linkUl', failures);
   }
-  else if (scoreData.inferences.linkUl) {
-    query.linkUlResult = testCrashText(deficitData.linkUl, 'linkUl');
+  else if (inferences.linkUl) {
+    query.linkUlResult = testCrashText(inferences.linkUl, 'linkUl');
   }
   else {
     query.linkUlResult = customSucceedText('linkUl');
   }
-  if (deficit.log) {
+  if (scores.log) {
     const {logCount, logSize, visitRejectionCount, prohibitedCount, visitTimeoutCount} = sourceData;
     const logData = {logCount, logSize, visitRejectionCount, prohibitedCount, visitTimeoutCount};
     const failures = customFailures(logData);
-    query.logResult = customResult(deficit.log, 'log', failures);
+    query.logResult = customResult(scores.log, 'log', failures);
   }
-  else if (scoreData.inferences.log) {
-    query.logResult = testCrashText(deficitData.log, 'log');
+  else if (inferences.log) {
+    query.logResult = testCrashText(inferences.log, 'log');
   }
   else {
     query.logResult = customSucceedText('log');
   }
-  if (deficit.menuNav) {
-    const failSource = testData.menuNav.result.totals;
+  if (scores.menuNav) {
+    const testAct = actOf('menuNav');
+    const failSource = testAct.result.totals;
     const failObj = {
       navigations: failSource.navigations.all.incorrect,
       menuItems: failSource.menuItems.incorrect,
       menus: failSource.menus.incorrect
     };
     const failures = customFailures(failObj);
-    query.menuNavResult = customResult(deficit.menuNav, 'menuNav', failures);
+    query.menuNavResult = customResult(scores.menuNav, 'menuNav', failures);
   }
-  else if (scoreData.inferences.menuNav) {
-    query.menuNavResult = testCrashText(deficitData.menuNav, 'menuNav');
+  else if (inferences.menuNav) {
+    query.menuNavResult = testCrashText(inferences.menuNav, 'menuNav');
   }
   else {
     query.menuNavResult = customSucceedText('menuNav');
   }
-  if (deficit.motion) {
-    const {result} = testData.motion;
+  if (scores.motion) {
+    const testAct = actOf('motion');
+    const {result} = testAct;
     result.bytes = result.bytes.join(', ');
     result.localRatios = result.localRatios.join(', ');
     result.pixelChanges = result.pixelChanges.join(', ');
     const failures = customFailures(result);
-    query.motionResult = customResult(deficit.motion, 'motion', failures);
+    query.motionResult = customResult(scores.motion, 'motion', failures);
   }
-  else if (scoreData.inferences.motion) {
-    query.motionResult = testCrashText(deficitData.motion, 'motion');
+  else if (inferences.motion) {
+    query.motionResult = testCrashText(inferences.motion, 'motion');
   }
   else {
     query.motionResult = customSucceedText('motion');
   }
-  if (deficit.radioSet) {
-    const failures = customFailures(testData.radioSet.result.totals);
-    query.radioSetResult = customResult(deficit.radioSet, 'radioSet', failures);
+  if (scores.radioSet) {
+    const testAct = actOf('radioSet');
+    const failures = customFailures(testAct.result.totals);
+    query.radioSetResult = customResult(scores.radioSet, 'radioSet', failures);
   }
-  else if (scoreData.inferences.radioSet) {
-    query.radioSetResult = testCrashText(deficitData.radioSet, 'radioSet');
+  else if (inferences.radioSet) {
+    query.radioSetResult = testCrashText(inferences.radioSet, 'radioSet');
   }
   else {
     query.radioSetResult = customSucceedText('radioSet');
   }
-  if (deficit.role) {
-    const {result} = testData.role;
+  if (scores.role) {
+    const testAct = actOf('role');
+    const {result} = testAct;
     delete result.tagNames;
     const failures = customFailures(result);
-    query.roleResult = customResult(deficit.role, 'role', failures);
+    query.roleResult = customResult(scores.role, 'role', failures);
   }
-  else if (scoreData.inferences.role) {
-    query.roleResult = testCrashText(deficitData.role, 'role');
+  else if (inferences.role) {
+    query.roleResult = testCrashText(inferences.role, 'role');
   }
   else {
     query.roleResult = customSucceedText('role');
   }
-  if (deficit.styleDiff) {
-    const {totals} = testData.styleDiff.result;
+  if (scores.styleDiff) {
+    const testAct = actOf('styleDiff');
+    const {totals} = testAct.result;
     const styleCounts = {};
     Object.keys(totals).forEach(key => {
       const data = totals[key];
@@ -285,40 +322,41 @@ exports.parameters = (report, query) => {
       styleCounts[key] = `${count} ${count === 1 ? 'style' : 'different styles'}`;
     });
     const failures = customFailures(styleCounts);
-    query.styleDiffResult = customResult(deficit.styleDiff, 'styleDiff', failures);
+    query.styleDiffResult = customResult(scores.styleDiff, 'styleDiff', failures);
   }
-  else if (scoreData.inferences.styleDiff) {
-    query.styleDiffResult = testCrashText(deficitData.styleDiff, 'styleDiff');
+  else if (inferences.styleDiff) {
+    query.styleDiffResult = testCrashText(inferences.styleDiff, 'styleDiff');
   }
   else {
     query.roleResult = customSucceedText('role');
   }
-  if (deficit.tabNav) {
-    const failSource = testData.tabNav.result.totals;
+  if (scores.tabNav) {
+    const testAct = actOf('tabNav');
+    const failSource = testAct.result.totals;
     const failObj = {
       navigations: failSource.navigations.all.incorrect,
       tabElements: failSource.tabElements.incorrect,
       tabLists: failSource.tabLists.incorrect
     };
     const failures = customFailures(failObj);
-    query.tabNavResult = customResult(deficit.tabNav, 'tabNav', failures);
+    query.tabNavResult = customResult(scores.tabNav, 'tabNav', failures);
   }
-  else if (scoreData.inferences.tabNav) {
-    query.tabNavResult = testCrashText(deficitData.tabNav, 'tabNav');
+  else if (inferences.tabNav) {
+    query.tabNavResult = testCrashText(inferences.tabNav, 'tabNav');
   }
   else {
     query.tabNavResult = customSucceedText('tabNav');
   }
-  if (deficit.zIndex) {
-    const {tagNames} = testData.zIndex.result.totals;
+  if (scores.zIndex) {
+    const testAct = actOf('zIndex');
+    const {tagNames} = testAct.result.totals;
     const failures = customFailures(tagNames);
-    query.zIndexResult = customResult(deficit.zIndex, 'zIndex', failures);
+    query.zIndexResult = customResult(scores.zIndex, 'zIndex', failures);
   }
-  else if (scoreData.inferences.zIndex) {
-    query.zIndexResult = testCrashText(deficitData.zIndex, 'zIndex');
+  else if (inferences.zIndex) {
+    query.zIndexResult = testCrashText(inferences.zIndex, 'zIndex');
   }
   else {
     query.zIndexResult = customSucceedText('zIndex');
   }
-  return query;
 };
